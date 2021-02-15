23f75b63917ad3aa3ed098c4ab053c87488b0cb9
JoanJuan10 <iijoanjuan10@gmail.com>
2021-02-15 10:19:09 +0100

    Ejercicio 2

---
+++ ex2/js/codigo.js
@@ -0,0 +1,5 @@
+// Pone 2 mensajes en consola
+var mensaje = "Hola Mundo!\nSoy el primer script!";
+var mensaje2 = "Hola Mundo!\nQué facil es incluir 'comillas simples'\ny \"comillas dobles\"";
+console.log(mensaje);
+alert(mensaje2);

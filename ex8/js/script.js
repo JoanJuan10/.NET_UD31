cec94592aab0099f393d692c39e85f031bbdc657
JoanJuan10 <iijoanjuan10@gmail.com>
2021-02-15 11:50:16 +0100

    Ejercicio 8

---
+++ ex8/js/script.js
@@ -0,0 +1,13 @@
+function calcularPAR(num) {
+    if (num % 2 == 0) {
+        alert("PAR");
+    }
+    else {
+        alert("IMPAR");
+    }
+}
+
+var numero = prompt("Introduce un entero");
+
+calcularPAR(numero);
+
